3ad479ac498c1eed19a52ad615bb8d562aa8e35b
Rishi9563 <rishiawasthi9563@gmail.com>
2025-06-18 10:38:32 +0530

    Create getcomputerChoice function
    getcomputerChoice generates random choice using math.random function , store it in computerSelection

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ function gethumanChoice(){
     humanSelection=humanChoice.toLowerCase();
 }
 
+function getcomputerChoice(){
+    let random=Math.random()*100;
+
+    if(random<35){
+        computerSelection="rock";
+    }
+
+    else if(random<69){
+        computerSelection="paper";
+    }
+
+    else {
+        computerSelection="scissors";
+    }
+}
+
+getcomputerChoice();
+alert(computerSelection);
