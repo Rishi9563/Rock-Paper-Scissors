3730ea2d36f6548b0c7ee1b877a636e2e1f69010
Rishi9563 <rishiawasthi9563@gmail.com>
2025-06-18 10:32:27 +0530

    Create function gethumanChoice in js file
    gethumanChoice takes input from user and connverts it to lowercase and stores it in universal variable(humanSelection)

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("hello world!");
+let humanSelection;
+let computerSelection;
+
+function gethumanChoice(){
+    let humanChoice=prompt("Enter your choice(Rock/Paper/Scissors)");
+    humanSelection=humanChoice.toLowerCase();
+}
+
