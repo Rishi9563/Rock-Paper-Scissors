07166a2ecf7328f94658488e367fbd5b7e4ced8e
Rishi9563 <rishiawasthi9563@gmail.com>
2025-08-11 21:43:25 +0530

    Modify as per our interfacescript.js file and link with gameUI.html file

--- script.js
+++ script.js
@@ -1,96 +1,177 @@
-let humanSelection;
+const btn = document.querySelector("#start");
+let userOption = document.getElementById("playerhand");
+let botOption = document.getElementById("bothand");
+const roundwinner = document.querySelector("#winner");
+const roundDisplay = document.querySelector("#rounddisplay");
+const playerScore = document.querySelector("#playerscore");
+const botScore = document.querySelector("#computerscore");
+const timerclock = document.querySelector(".timer");
+let rounds = 0;
+let humanScore = 0;
+let computerScore = 0;
 let computerSelection;
-let humanScore=0;
-let computerScore=0;
+let humanSelection;
 
-playGame();
 
-function gethumanChoice(){
-    let humanChoice=prompt("Enter your choice(Rock/Paper/Scissors)");
-    humanSelection=humanChoice.toLowerCase();
-}
 
-function getcomputerChoice(){
-    let random=Math.random()*100;
+function getcomputerChoice() {
 
-    if(random<35){
-        computerSelection="rock";
+    if ((humanScore == 5) || (computerScore == 5)) {
+        return;
     }
 
-    else if(random<69){
-        computerSelection="paper";
+    let random = Math.random() * 100;
+
+    if (random < 35) {
+        computerSelection = "rock";
+    }
+
+    else if (random < 69) {
+        computerSelection = "paper";
     }
 
     else {
-        computerSelection="scissors";
+        computerSelection = "scissors";
+    }
+    other();
+}
+
+function other() {
+    if (computerSelection == "rock") {
+        botOption.src = "images/noun-rock-88661 - Copy.png";
+    }
+
+    else if (computerSelection == "paper") {
+        botOption.src = "images/papercopy.png";
+    }
+
+    else {
+        botOption.src = "images/noun-scissors-88666 - Copy.png";
     }
 }
 
-function playRound(){
-    gethumanChoice();
+function compare(playerchoice) {
+    if ((humanScore == 5) || (computerScore == 5)) {
+        return;
+    }
+
     getcomputerChoice();
-    
-    console.log("You chose : %s  ||   Computer chose : %s",humanSelection,computerSelection);
 
-    if(humanSelection==computerSelection){
-        console.log("Round Draw!");
+    if (playerchoice == computerSelection) {
+        roundwinner.textContent = "Draw!";
     }
 
-    else if(humanSelection=="paper" && computerSelection=="rock"){
-        console.log("You win!");
-        humanScore++;
+    else if (playerchoice == "rock" && computerSelection == "paper") {
+        roundwinner.textContent = "BOT Wins!";
+        computerScore += 1;
     }
 
-    else if(humanSelection=="rock" && computerSelection=="scissors"){
-        console.log("You win!");
-        humanScore++;
+    else if (playerchoice == "rock" && computerSelection == "scissors") {
+        roundwinner.textContent = "You Win!";
+        humanScore += 1;
     }
 
-    else if(humanSelection=="scissors" && computerSelection=="paper"){
-        console.log("You win!");
-        humanScore++;
+    else if (playerchoice == "paper" && computerSelection == "rock") {
+        roundwinner.textContent = "You Win!";
+        humanScore += 1;
     }
 
-    else if(humanSelection=="rock" && computerSelection=="paper"){
-        console.log("Computer wins!");
-        computerScore++;
+    else if (playerchoice == "paper" && computerSelection == "scissors") {
+        roundwinner.textContent = "BOT Wins!";
+        computerScore += 1;
     }
 
-    else if(humanSelection=="paper" && computerSelection=="scissors"){
-        console.log("Computer wins!");
-        computerScore++;
+    else if (playerchoice == "scissors" && computerSelection == "rock") {
+        roundwinner.textContent = "BOT Wins!";
+        computerScore += 1;
     }
 
-    else if(humanSelection=="scissors" && computerSelection=="rock"){
-        console.log("Computer wins!");
-        computerScore++;
+    else if (playerchoice == "scissors" && computerSelection == "paper") {
+        roundwinner.textContent = "You Win!";
+        humanScore += 1;
     }
 
-    else {
-        console.log("Sommething went wrong!");
-    }
+    scoreUpdate();
+}
 
-    console.log("You : %d                  Computer : %d",humanScore,computerScore);
-    console.log("\n");
+function scoreUpdate() {
+    rounds += 1;
+    roundDisplay.textContent = `Round ${rounds}`;
+    playerScore.textContent = humanScore;
+    botScore.textContent = computerScore;
+
+    if ((humanScore === 5) || (computerScore === 5)) {
+        finalDisplay();
+        return;
+    }
 
 }
 
-function playGame(){
-    for(let i=1;i<=5;i++){
-        console.log("Round - %d\n",i);
-        playRound();
+function playRound(playerchoice) {
+    if ((humanScore === 5) || (computerScore === 5)) {
+        return;
     }
 
-    if(humanScore==computerScore){
-        console.log("Game Draw!");
+    if (playerchoice == "rock") {
+        userOption.src = "images/noun-rock-88661.png";
+        
     }
 
-    else if(humanScore>computerScore){
-        console.log("You win the game!");
+    else if (playerchoice == "paper") {
+        userOption.src = "images/noun-paper-88662.png";
     }
 
     else {
-        console.log("Computer wins the game!");
+        userOption.src = "images/noun-scissors-88666.png";
+    }
+
+    getcomputerChoice();
+    compare(playerchoice);
+}
+
+function timer(){
+    let currTime = new Date().getTime();
+    while(new Date().getTime() - currTime < 500){
     }
 }
+
+function finalDisplay() {
+    let winner = (humanScore > computerScore) ? "Player" : "BOT";
+    roundDisplay.textContent = `${winner} wins the game!`;
+    roundwinner.textContent = "Game Ends!";
+    userOption.src = "images/noun-rock-88661.png";
+    botOption.src = "images/noun-rock-88661 - Copy.png";
+}
+
+function playerchoice() {
+
+    document.querySelector("#rock").addEventListener("click", function () {
+        playRound("rock");
+    });
+
+    document.querySelector("#paper").addEventListener("click", function () {
+        playRound("paper");
+    });
+
+    document.querySelector("#scissors").addEventListener("click", function () {
+        playRound("scissors");
+    });
+
+}
+
+
+playerchoice();
+
+
+btn.addEventListener("click" , function(){
+    rounds = 0;
+    humanScore = 0;
+    computerScore = 0;
+    roundDisplay.textContent = "Let's begin!";
+    playerScore.textContent = humanScore;
+    botScore.textContent = computerScore;
+    roundwinner.textContent = "";
+    userOption.src = "images/noun-rock-88661.png";
+    botOption.src = "images/noun-rock-88661 - Copy.png";
+});
 
